ed87bc0a86db96196ccd197c172ece52d442215e
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-09 22:02:15 +0200

    comment added for testing purposes

--- core/core/urls.py
+++ core/core/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# comment added for testing purpose
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/auth/',include('auth_app.api.urls'))
