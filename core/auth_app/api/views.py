14c4664a2877398b14d57b9236f0b6bf21ff73db
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-12 21:37:27 +0200

    feat: redirect added in case of a succesfull registration

--- core/auth_app/api/views.py
+++ core/auth_app/api/views.py
@@ -1,5 +1,5 @@
 # app/auth_user_views.py
-
+from django.shortcuts import render, redirect
 from .serializers import RegistrationSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import status
@@ -7,49 +7,29 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
 
 class RegistrationView(APIView):
     permission_classes=[AllowAny]
+    parser_classes = [JSONParser, FormParser, MultiPartParser]
 
     def post(self, request):
         serializer = RegistrationSerializer(data=request.data)
 
         if not serializer.is_valid():
             return Response(serializer.errors, status=400)
-        
-        # Validate and create the user *before* issuing tokens
-
         try:
             user_data = serializer.save() # This automatically handles password hashing
-
             token, created = Token.objects.get_or_create(user=user_data)
             data = {
                 'token': token.key,
                 'username': user_data.username,
                 'email': user_data.email
-                # Consider adding additional fields like name if needed later
             }
         except Exception as e:
             return Response({'error': str(e)}, status=500)
-        
-        return Response(data, status=201) # Use 201 Created status on successful registration 
-
-    # def post(self, request):
-    #     serializer = RegistrationView(data=request.data)
-    #     data = {}
-
-    #     if serializer.is_valid():
-    #         saved_account = serializer.save()
-    #         token, created = Token.objects.get_or_create(user=saved_account)
-    #         data = {
-    #             'token': token.key,
-    #             'username': saved_account.username,
-    #             'email': saved_account.email
-    #         }
-    #     else:
-    #         data=serializer.errors
-    #     return Response(data)
-    
+        return redirect('../login')
+        # return Response(data, status=201)
 
 class CustomLoginView(APIView):
     permission_classes = [AllowAny]
@@ -66,3 +46,12 @@ class CustomLoginView(APIView):
             }
             return Response(data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# Testpasswort für Copy and Paste
+
+# {
+#     "username": "Mirko123",
+#     "email": "mirko@testmail.de",
+#     "password": "passwort24!",
+#     "repeated_password": "passwort24!"
+# }
