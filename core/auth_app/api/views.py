bbcc09f70b5d5582ffb350f4b5c70748a9f430ca
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-13 21:58:39 +0200

    minor changes to views.py file in the auth_app

--- core/auth_app/api/views.py
+++ core/auth_app/api/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render, redirect
 from .serializers import RegistrationSerializer
 from rest_framework.permissions import AllowAny
 from rest_framework import status
@@ -7,33 +6,28 @@ from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.serializers import AuthTokenSerializer
 from rest_framework.parsers import JSONParser, FormParser, MultiPartParser
-from rest_framework import generics
+from rest_framework import generics, status
 
-class RegistrationView(APIView):
-    permission_classes=[AllowAny]
-    parser_classes = [JSONParser, FormParser, MultiPartParser]
+
+class RegistrationView(generics.CreateAPIView):
+    serializer_class = RegistrationSerializer
+    permission_classes = [AllowAny]
 
     def get(self, request):
         return Response({"message": "Registration form"})
 
-    def post(self, request):
-        serializer_class = RegistrationSerializer
-        serializer = serializer_class(data=request.data)
-
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        try:
-            user_data = serializer.save()
-            token, created = Token.objects.get_or_create(user=user_data)
-            data = {
-                'token': token.key,
-                'username': user_data.username,
-                'email': user_data.email
-            }
-            return Response(data, status=201)
-        except Exception as e:
-            return Response({'error': str(e)}, status=500)
-        
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        token, created = Token.objects.get_or_create(user=user)
+        data = {
+            'token': token.key,
+            'username': user.username,
+            'email': user.email
+        }
+        return Response(data, status=201)
+    
 class CustomLoginView(APIView):
     permission_classes = [AllowAny]
 
