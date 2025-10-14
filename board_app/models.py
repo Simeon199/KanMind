0d0c19b36af259e28887954d60213fa53878bf61
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-14 21:48:27 +0200

    feat: Boardmodel added to models.py of board_app

--- board_app/models.py
+++ board_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Boardmodel(models.Model):
+    title = models.CharField(max_length=255)
+    member_count = models.PositiveIntegerField()
+    ticket_count = models.PositiveIntegerField()
+    tasks_to_do_count = models.PositiveBigIntegerField()
+    tasks_high_prio_count = models.PositiveBigIntegerField()
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_board')
+
+    def __str__(self):
+        return self.title
