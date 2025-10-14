c00df6c619c44e77d70723f882253c0e3159abcd
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-14 10:57:51 +0200

    new apps added to project structure

--- core/core/settings.py
+++ core/core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
-    'auth_app'
+    'auth_app',
+    'board_app',
+    'tasks_app'
 ]
 
 MIDDLEWARE = [
