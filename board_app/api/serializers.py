633430efb05fb5500185a63131f2ad96113430d6
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-20 19:36:02 +0200

    saved changes along with new requirements.txt file

--- board_app/api/serializers.py
+++ board_app/api/serializers.py
@@ -6,37 +6,3 @@ class BoardSerializer(serializers.ModelSerializer):
         class Meta:
             model = Board
             fields = ['title', 'members']
-
-# class BoardSerializer(serializers.ModelSerializer):
-#     members = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
-#     owner_id = serializers.IntegerField(source='owner_id', read_only=True)
-#     member_count = serializers.SerializerMethodField(read_only=True)
-#     ticket_count = serializers.IntegerField(read_only=True)
-#     tasks_to_do_count = serializers.IntegerField(read_only=True)
-#     tasks_high_prio_count = serializers.IntegerField(read_only=True)
-#     id = serializers.IntegerField(read_only=True)
-    
-#     class Meta:
-#         model = Board
-#         fields = [
-#             'id', 'title', 'members', 'member_count', 'ticket_count',
-#             'tasks_to_do_count', 'tasks_high_prio_count', 'owner_id'
-#         ]
-
-#     def get_member_count(self, obj):
-#         return obj.members.count()
-    
-#     def create(self, validated_data):
-#         members = validated_data.pop('members', [])
-#         owner = self.context['request'].user
-#         board = Board.objects.create(
-#             owner=owner,
-#             title=validated_data.get('title'),
-#             ticket_count=validated_data.get('ticket_count', 0),
-#             tasks_to_do_count=validated_data.get('tasks_to_do_count', 0),
-#             tasks_high_prio_count=validated_data.get('tasks_high_prio_count', 0)
-#         )
-#         board.members.set(members)
-#         board.member_count = board.members_count()
-#         board.save()
-#         return board
--- board_app/api/views.py
+++ board_app/api/views.py
@@ -7,31 +7,15 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework import exceptions
 from django.utils.translation import gettext_lazy as _
 
-# class TokenOwnerAuthentication(TokenAuthentication):
-
-#     def authenticate(self, request):
-#         if not request.user.is_superuser:
-#             msg = _('User must be Admin!')
-#             raise exceptions.AuthenticationFailed(msg)
-#         return super().authenticate(request)
-
 class BoardView(generics.ListCreateAPIView):
     queryset = Board.objects.all()
     serializer_class = BoardSerializer
-    permission_classes = [IsAuthenticated] # OwnerOfBoardPermission
+    permission_classes = [IsAuthenticated, OwnerOfBoardPermission] # OwnerOfBoardPermission
 
-    # def get_queryset(self):
-    #     user = self.request.user
-    #     return Board.objects.filter(owner=user) | Board.objects.filter(members=user)
-    
-    # def perform_create(self, serializer):
-    #     return serializer.save(owner=self.request.user)
+class BoardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Board.objects.all()
+    serializer_class = BoardSerializer
+    permission_classes = [IsAuthenticated, OwnerOfBoardPermission] 
 
-
-# class BoardRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Board.objects.all()
-#     serializer_class = BoardSerializer
-#     permission_classes = [IsAuthenticated, OwnerOfBoardPermission] 
-
-#     def perform_update(self, serializer):
-#         serializer.save(owner=self.request.user) 
+    def perform_update(self, serializer):
+        serializer.save(owner=self.request.user) 
