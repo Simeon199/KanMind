e31e4704a2180cdccdc0569a7c82a186eb0cb5a0
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-23 12:22:13 +0200

    updates saved

--- board_app/api/serializers.py
+++ board_app/api/serializers.py
@@ -4,32 +4,47 @@ from django.contrib.auth.models import User
 
 class BoardSerializer(serializers.ModelSerializer):
         owner_id = serializers.SerializerMethodField()
-
-        #new lines
         members = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(), many=True)
         member_count = serializers.SerializerMethodField()
-        ticket_count = serializers.IntegerField(read_only=True)
-        tasks_to_do_count = serializers.IntegerField(read_only=True)
-        tasks_high_prio_count = serializers.IntegerField(read_only=True)
+        # ticket_count = serializers.SerializerMethodField()
+        # tasks_to_do_count = serializers.SerializerMethodField()
+        # tasks_high_prio_count = serializers.SerializerMethodField()
+        # ticket_count = serializers.IntegerField(read_only=True)
+        # tasks_to_do_count = serializers.IntegerField(read_only=True)
+        # tasks_high_prio_count = serializers.IntegerField(read_only=True)
         id=serializers.IntegerField(read_only=True)
 
         class Meta:
             model = Board
-            fields = [ # 'id'
-                  'id', 'title', 'members', 'member_count', 'ticket_count',
-                  'tasks_to_do_count', 'tasks_high_prio_count', 'owner_id'
+            fields = [
+                  'id', 'title', 'members', 'member_count', 'owner_id'
             ]
 
         def get_owner_id(self, obj: Board):
               return obj.owner.id
         
-        def get_member_count(self, obj):
+        def get_member_count(self, obj: Board):
+              if not obj.members.exists():
+                    return 0
               return obj.members.count()
         
-        def create(self, validated_data):
-              members = validated_data.pop('members', [])
-              board = Board.objects.create(**validated_data)
-              board.members.set(members)
-              board.member_count = board.members.count()
-              board.save()
-              return board
+      #   def get_ticket_count(self, obj:Board):
+      #         if not obj.ticket_count:
+      #               return 0
+      #         return obj.ticket_count
+        
+      #   def get_tasks_to_do_count(self, obj:Board):
+      #         if not obj.tasks_to_do_count:
+      #               return 0
+      #         return obj.tasks_to_do_count
+        
+      #   def get_tasks_high_prio_count(self, obj:Board):
+      #         if not obj.tasks_high_prio_count:
+      #               return 0
+      #         return obj.tasks_high_prio_count
+        
+      #   def create(self, validated_data):
+      #         members = validated_data.pop('members', [])
+      #         board = Board.objects.create(**validated_data)
+      #         board.members.set(members)
+      #         return board
--- board_app/models.py
+++ board_app/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import User
 
 class Board(models.Model):
     title = models.CharField(max_length=255)
-    member_count = models.PositiveIntegerField()
-    ticket_count = models.PositiveIntegerField()
-    tasks_to_do_count = models.PositiveBigIntegerField()
-    tasks_high_prio_count = models.PositiveBigIntegerField()
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_board')
-    members = models.ManyToManyField(User, related_name='boards') # new line
+    # ticket_count = models.PositiveIntegerField()
+    # tasks_to_do_count = models.PositiveBigIntegerField()
+    # tasks_high_prio_count = models.PositiveBigIntegerField()
+    # owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_board')
+    members = models.ManyToManyField(User, related_name='boards')
 
     def __str__(self):
-        return self.title
+        return f"{self.title}{self.members}"
+    
