2eb6a00c736fef1362d159f48e67b1f3ab2bfc3b
Simon Kiesner <164670179+Simeon199@users.noreply.github.com>
2025-10-30 21:29:56 +0100

    added changes in views.py and urls.py of the tasks_app

--- tasks_app/api/urls.py
+++ tasks_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TasksAssignedView, TaskListCreateView
+from .views import TasksAssignedOrReviewedView, TaskListCreateView
 
 urlpatterns = [
     path('', TaskListCreateView.as_view(), name='task'),
-    path('assigned-to-me/', TasksAssignedView.as_view(), name='tasks'),
+    path('assigned-to-me/', TasksAssignedOrReviewedView.as_view(), name='tasks'),
+    path('reviewing/', TasksAssignedOrReviewedView().as_view(), name='tasks')
 ]
--- tasks_app/api/views.py
+++ tasks_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .serializers import TaskSerializer
 
-class TasksAssignedView(generics.ListCreateAPIView):
+class TasksAssignedOrReviewedView(generics.ListCreateAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
     permission_classes = [IsAuthenticated]
@@ -17,11 +17,10 @@ class TaskListCreateView(generics.ListCreateAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        # Show tasks where user is a board member or assignee/reviewer
         return Task.objects.filter(board__members=user).distinct()
     
-    def perform_create(self, serializer):
-        serializer.save() # validation already enforces members
+    # def perform_create(self, serializer):
+    #     serializer.save()
 
 class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
@@ -29,6 +28,5 @@ class TaskRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # ensure user can access this object
         user = self.request.user
         return Task.objects.filter(board__members=user).distinct()
